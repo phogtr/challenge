ddf97cef386a17b6712290042dfb5f8c7d48eabb
phogtr <phogbtr@gmail.com>
2021-08-22 00:27:38 -0700

    production build

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.tsx",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "../../build"),
+    path: path.resolve(__dirname, "../../public"),
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
