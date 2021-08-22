b58d3e0d9093fbbecacb67da61e0282ddbf21ded
phogtr <phogbtr@gmail.com>
2021-08-21 17:42:41 -0700

    initial client setup

---
+++ src/client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/index.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Initial setup</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/client/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../../build"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
