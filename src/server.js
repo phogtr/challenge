95e31dcfb76bd285af4c25084936c5c3e499161c
phogtr <phogbtr@gmail.com>
2021-08-21 18:30:27 -0700

    install cors for backend server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const app = express();
-const registerRoutes = require('./routes');
-const path = require('path');
+const registerRoutes = require("./routes");
+const path = require("path");
+const cors = require("cors");
 
 // server config
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 // serves static files
 app.use(express.static(path.join(__dirname, "../public")));
@@ -15,17 +18,15 @@ registerRoutes(app);
 
 // create server start method
 const start = () => {
-    return new Promise((resolve, reject) => {
-        // start the server
-        app.listen(port, () => {
-            console.log(`Connected to Port ${port}`);
-            resolve()
-        });
-    }).catch((error) => {
-        console.log(`failed to start server => ${error.message}`)
+  return new Promise((resolve, reject) => {
+    // start the server
+    app.listen(port, () => {
+      console.log(`Connected to Port ${port}`);
+      resolve();
     });
-}
+  }).catch((error) => {
+    console.log(`failed to start server => ${error.message}`);
+  });
+};
 
 module.exports = start;
-
-
